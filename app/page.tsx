63f754cbbdbda70bed6a71d7cb7fe3e72783bbe8
Lemos <lemosjvd@gmail.com>
2026-04-16 15:17:49 -0300

    Atualiza page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -572,7 +572,7 @@ export default function Home() {
                   <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line>
                 </svg>
               </a>
-              <a href="www.linkedin.com/in/deborah-fegaduoli-lemos-398105244" className="social-icon" aria-label="LinkedIn">
+              <a href="https://www.linkedin.com/in/deborah-fegaduoli-lemos-398105244" className="social-icon" aria-label="LinkedIn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                   <rect width="4" height="12" x="2" y="9"></rect>
